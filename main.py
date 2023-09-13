b6ef31bab9bd78c4cd14b536597c370e6f48bd21
Alex Svetoslavov <451875@student.fontys.nl>
2023-09-13 19:50:04 +0200

    added base model

--- main.py
+++ main.py
@@ -11,3 +11,40 @@ with Live(save_dvc_exp=True) as live:
         live.log_metric("val/accuracy",epoch + random.random() )
         live.log_metric("val/loss", epochs - epoch - random.random())
         live.next_step()
+
+
+
+
+
+#import necessary libraries
+import tensorflow as tf
+
+#load training data and split into train and test sets
+mnist = tf.keras.datasets.mnist
+
+(x_train,y_train), (x_test,y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+
+#define model
+model = tf.keras.models.Sequential([
+                               tf.keras.layers.Flatten(input_shape=(28,28)),
+                                   tf.keras.layers.Dense(128,activation='relu'),
+                                   tf.keras.layers.Dropout(0.2),
+                                   tf.keras.layers.Dense(10)
+])
+
+#define loss function variable
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+#define optimizer,loss function and evaluation metric
+model.compile(optimizer='adam',
+             loss=loss_fn,
+             metrics=['accuracy'])
+
+#train the model
+model.fit(x_train,y_train,epochs=5)
+
+
+#test model accuracy on test set
+model.evaluate(x_test,y_test,verbose=2)
