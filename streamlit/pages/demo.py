8e529ef621a58ccf5fed33ffe0558276418c0040
Alex Svetoslavov <xfowth@gmail.com>
2024-01-22 13:14:59 +0100

    Added demo demo

--- streamlit/pages/demo.py
+++ streamlit/pages/demo.py
@@ -11,14 +11,24 @@ model = joblib.load('./model.pkl')
 # Load your data
 data = pd.read_csv('./data/test_data.csv')
 
-st.text("This is a test")
+benign = data[data['label'] == 0]
+malicious = data[data['label'] == 1]
+
+# Keep 20% of the benign data
+benign_sample = benign.sample(frac=0.07, random_state=1)
+
+# Concatenate the benign sample and the malicious data
+data = pd.concat([benign_sample, malicious])
+
+st.text("This is not a test")
+
 
 # Ensure the order of columns matches the order of features in the trained model
 data = data[model.feature_names_in_]
 
 # Function to highlight malicious
 def highlight_malicious(val):
-    color = 'red' if val == 'malicious' else 'green'
+    color = 'red' if val == '1' else 'green'
     return 'background-color: %s' % color
 
 # Create a placeholder for the table
@@ -30,7 +40,12 @@ num_rows = 1
 # Update the table in the placeholder
 for i in range(len(data)):
     # Make prediction for the current row
-    prediction = model.predict(data[i:i+1].drop(columns=['prediction'], errors='ignore'))
+    if 'prediction' in data.columns:
+        # Make prediction for the current row
+        prediction = model.predict(data[i:i+1].drop(columns=['prediction']))
+    else:
+        # Make prediction for the current row
+        prediction = model.predict(data[i:i+1])
     
     # Add prediction to the data
     data.loc[i, 'prediction'] = prediction[0]
@@ -39,4 +54,4 @@ for i in range(len(data)):
     table_placeholder.write(data[:i+1].style.applymap(highlight_malicious))
     
     # Wait for 2 seconds before updating the table with the next row
-    time.sleep(1)
+    time.sleep(0.5)
