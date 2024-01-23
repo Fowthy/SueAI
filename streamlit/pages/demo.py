3e6ad07afc832a4b46554e212c576f013e128c63
Alex Svetoslavov <xfowth@gmail.com>
2024-01-23 12:45:31 +0100

    Added notebooks in notebooks

--- streamlit/pages/demo.py
+++ streamlit/pages/demo.py
@@ -15,7 +15,7 @@ benign = data[data['label'] == 0]
 malicious = data[data['label'] == 1]
 
 # Keep 20% of the benign data
-benign_sample = benign.sample(frac=0.07, random_state=1)
+benign_sample = benign.sample(frac=0.01, random_state=1)
 
 # Concatenate the benign sample and the malicious data
 data = pd.concat([benign_sample, malicious])
@@ -25,6 +25,8 @@ st.text("This is not a test")
 
 # Ensure the order of columns matches the order of features in the trained model
 data = data[model.feature_names_in_]
+
+data.dropna()
 
 # Function to highlight malicious
 def highlight_malicious(val):
@@ -37,21 +39,17 @@ table_placeholder = st.empty()
 # Number of rows to display at a time
 num_rows = 1
 
+predicted_data = data.copy()
+
 # Update the table in the placeholder
 for i in range(len(data)):
-    # Make prediction for the current row
-    if 'prediction' in data.columns:
-        # Make prediction for the current row
-        prediction = model.predict(data[i:i+1].drop(columns=['prediction']))
-    else:
-        # Make prediction for the current row
-        prediction = model.predict(data[i:i+1])
-    
+    prediction = model.predict(data[i:i+1])
+        
     # Add prediction to the data
-    data.loc[i, 'prediction'] = prediction[0]
+    predicted_data.loc[i, 'prediction'] = prediction[0] if prediction else 1
     
     # Update the table in the placeholder
-    table_placeholder.write(data[:i+1].style.applymap(highlight_malicious))
+    table_placeholder.write(predicted_data[:i+1].style.applymap(highlight_malicious))
     
     # Wait for 2 seconds before updating the table with the next row
     time.sleep(0.5)
